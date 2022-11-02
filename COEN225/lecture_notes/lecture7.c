3fd9eb4eade82d16631c361f226c1d7627b96592
Stephen Tambussi <stevetambussi@gmail.com>
2022-11-01 19:13:12 -0700

    Started lecture 7 notes

---
+++ COEN225/lecture_notes/lecture7.c
@@ -0,0 +1,40 @@
+/*  
+*   Stephen Tambussi
+*   COEN225
+*   Lecture 7
+*   11/1/22
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    return 0;
+}
+
+/*
+*   Format String Vulnerabilities
+*   -----------------------------------------
+*   Duo Factor Authentication App Exploit
+*   - TODO
+*
+*   Using printf to overwrite GOT entry
+*   - TODO
+*   
+*   Homework 4 Discussion
+*   -----------------------------------------
+*   Dissecting GCC
+*   - TODO
+*
+*   Shared/Dynamic Linking vs Static Linking
+*   - TODO
+*
+*   Intercepting dynamic library functions
+*   - TODO
+*   
+*   Displaying the runtime stack programmatically
+*   - TODO
+*
+*   The check_leak.c starter code
+*   - TODO
+*/
